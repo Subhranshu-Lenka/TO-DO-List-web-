37713326f5aef171d7cd981052db54c1ac18f0ee
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2024-01-22 16:33:10 +0530

    added the checked and remove functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const addBtn = document.getElementById("add-btn");
 const listContainer = document.getElementById("list-container");
+const lis = document.querySelectorAll("li");
 
 addBtn.addEventListener("click", () => {
     addTask();
@@ -15,19 +16,20 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+
+    inputBox.value = "";
 }
 
-
-
-// button.addEventListener("click", function () {
-//     // random num generate
-//     // change the dice acc to num
-//     // check for winner
-//     // if draw try again
-
-//     let obj = generateNum();
-//     changeDice(obj);
-//     checkWinner(obj);
-
-// });
+listContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+})
